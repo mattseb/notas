5f788503840aef9d7ce8d697f973517e88565809
mattseb <mateosmv1608@gmail.com>
2023-01-05 17:46:14 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:notas/db/notes_database.dart';
+import 'package:notas/screens/notes_screen.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Notes SQLite';
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: NotesPage(),
+      );
+}
---
+++ lib/widget/note_form_widget.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class NoteFormWidget extends StatelessWidget {
+  final bool? isImportant;
+  final int? number;
+  final String? title;
+  final String? description;
+  final String categoria;
+  final ValueChanged<bool> onChangedImportant;
+  final ValueChanged<int> onChangedNumber;
+  final ValueChanged<String> onChangedTitle;
+  final ValueChanged<String> onChangedDescription;
+  final ValueChanged<String?>? onChangedCategoria;
+
+  const NoteFormWidget({
+    Key? key,
+    this.isImportant = false,
+    this.number = 0,
+    this.title = '',
+    this.description = '',
+    this.categoria = '',
+    required this.onChangedImportant,
+    required this.onChangedNumber,
+    required this.onChangedTitle,
+    required this.onChangedDescription,
+    required this.onChangedCategoria,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Switch(
+                    value: isImportant ?? false,
+                    onChanged: onChangedImportant,
+                  ),
+                  Expanded(
+                    child: Slider(
+                      value: (number ?? 0).toDouble(),
+                      min: 0,
+                      max: 5,
+                      divisions: 5,
+                      onChanged: (number) => onChangedNumber(number.toInt()),
+                    ),
+                  )
+                ],
+              ),
+              buildTitle(),
+              SizedBox(height: 8),
+              buildCategoria(),
+              SizedBox(height: 8),
+              buildDescription(),
+              SizedBox(height: 16),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildTitle() => TextFormField(
+        maxLines: 1,
+        initialValue: title,
+        style: TextStyle(
+          color: Colors.white70,
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Título',
+          hintStyle: TextStyle(color: Colors.white70),
+        ),
+        validator: (title) => title != null && title.isEmpty
+            ? 'El título no debe estar bacio'
+            : null,
+        onChanged: onChangedTitle,
+      );
+
+  Widget buildCategoria() => DropdownButton<String>(
+        value: categoria,
+        icon: Icon(Icons.arrow_downward),
+        iconSize: 24,
+        elevation: 16,
+        style: TextStyle(color: Colors.deepPurple),
+        underline: Container(
+          height: 2,
+          color: Colors.deepPurpleAccent,
+        ),
+        onChanged: onChangedCategoria,
+        items: <String>['Urgente', 'Media', 'Normal', "No hay"]
+            .map<DropdownMenuItem<String>>((String valor) {
+          return DropdownMenuItem<String>(
+            value: valor,
+            child: Text(valor),
+          );
+        }).toList(),
+      );
+
+  Widget buildDescription() => TextFormField(
+        maxLines: 5,
+        initialValue: description,
+        style: TextStyle(color: Colors.white60, fontSize: 18),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Descrioción de la nota...',
+          hintStyle: TextStyle(color: Colors.white60),
+        ),
+        validator: (title) => title != null && title.isEmpty
+            ? 'La descripción no puede estar bacia'
+            : null,
+        onChanged: onChangedDescription,
+      );
+}
